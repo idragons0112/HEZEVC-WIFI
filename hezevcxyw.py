7f47122e09e0b44700d862313dcde7e6137af5bd
idragons0112 <108582074+idragons0112@users.noreply.github.com>
2022-11-17 16:12:18 +0800

    Update hezevcxyw.py

--- hezevcxyw.py
+++ hezevcxyw.py
@@ -13,9 +13,9 @@ if code == 200:  # get值200，则正确打开
     driver.maximize_window()  # 最大化窗口
     time.sleep(1)  # 等待一秒
     driver.find_element(By.ID, "id_userName").click()  # 找到用户名输入窗口
-    driver.find_element(By.ID, "id_userName").send_keys('19713041150')  # 输入账号
+    driver.find_element(By.ID, "id_userName").send_keys('此处输入账号')  # 输入账号
     driver.find_element(By.ID, "id_userName").send_keys(Keys.TAB)  # 换行到账号密码窗口
-    driver.find_element(By.ID, "id_userPwd").send_keys('lwg15000')  # 输入密码
+    driver.find_element(By.ID, "id_userPwd").send_keys('此处输入密码')  # 输入密码
     time.sleep(1)  # 等待一秒（可以去除此代码）
     driver.find_element(By.ID, "id_userPwd").send_keys(Keys.ENTER)  # 登陆
     driver.quit()  # 退出selenium指令集
