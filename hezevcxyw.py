57d026c66bb85c0ba78abb1a7046506d91589e29
idragons0112 <108582074+idragons0112@users.noreply.github.com>
2022-11-17 16:03:40 +0800

    Update hezevcxyw.py

--- hezevcxyw.py
+++ hezevcxyw.py
@@ -4,18 +4,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from urllib.request import urlopen
 from selenium.webdriver.common.by import By
-resp=urlopen("http://10.1.0.1:8080/byod/templatePage/20190620143747626/guestRegister.jsf")
-code=resp.getcode()
-if code==200:
-    driver=webdriver.Edge("C:\Program Files (x86)\Microsoft\Edge\Application\msedgedriver.exe")
-    driver.get("http://10.1.0.1:8080/byod/templatePage/20190620143747626/guestRegister.jsf")
-    driver.maximize_window()
-    time.sleep(1)
-    driver.find_element(By.ID,"id_userName").click()
-    driver.find_element(By.ID,"id_userName").send_keys('此处填写校园网账号')
-    driver.find_element(By.ID,"id_userName").send_keys(Keys.TAB)
-    driver.find_element(By.ID,"id_userPwd").send_keys('此处填写校园网密码')
-    driver.find_element(By.ID,"id_userPwd").send_keys(Keys.ENTER)
-    driver.quit()
+time.sleep(5)  # 开机等待时间
+resp = urlopen("http://10.1.0.1:8080/byod/templatePage/20190620143747626/guestRegister.jsf")  # 打开校园网登陆界面
+code = resp.getcode()
+if code == 200:  # get值200，则正确打开
+    driver = webdriver.Edge("C:\Program Files (x86)\Microsoft\Edge\Application\msedgedriver.exe")  # 调用浏览器驱动程序
+    driver.get("http://10.1.0.1:8080/byod/templatePage/20190620143747626/guestRegister.jsf")  # 打开校园网登陆界面
+    driver.maximize_window()  # 最大化窗口
+    time.sleep(1)  # 等待一秒
+    driver.find_element(By.ID, "id_userName").click()  # 找到用户名输入窗口
+    driver.find_element(By.ID, "id_userName").send_keys('19713041150')  # 输入账号
+    driver.find_element(By.ID, "id_userName").send_keys(Keys.TAB)  # 换行到账号密码窗口
+    driver.find_element(By.ID, "id_userPwd").send_keys('lwg15000')  # 输入密码
+    time.sleep(1)  # 等待一秒（可以去除此代码）
+    driver.find_element(By.ID, "id_userPwd").send_keys(Keys.ENTER)  # 登陆
+    driver.quit()  # 退出selenium指令集
 else:
-    sys.exit()
+    sys.exit()  # 如果get值不为200，则退出此程序
